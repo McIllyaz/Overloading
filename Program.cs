31464c266753e38b58da31cc853495b2ffe8d480
Illyaz Arya <71999096+McIllyaz@users.noreply.github.com>
2023-04-13 19:34:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers  number = new Numbers();
+
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3, 1));
+            Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 2, 5.3f));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #2: {0}", number.FindMaximum(3, 9, 10.5f));
+
+        }
+    }
+}
